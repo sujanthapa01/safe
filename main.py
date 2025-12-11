7ff7b859684111454119b4fb6787dc0c52f5a1cd
Sujan Thapa <sujanthapa4001@gmail.com>
2025-12-12 00:32:35 +0530

    entry point

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hi my name is sujan")
+from services import create_user_service, login_user_service
+import inquirer
+
+options = [
+    inquirer.List(
+        "action",
+        choices=["register_user", "login_user"]
+    )
+]
+
+choosen = inquirer.prompt(options)
+
+action_map ={
+"register_user": create_user_service,
+"login_user": login_user_service
+}
+
+action_map[choosen["action"]]()
