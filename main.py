f96f7dc98874c1b483493b1ba434eae5bd6f0045
Sujan Thapa <sujanthapa4001@gmail.com>
2025-12-14 13:38:58 +0530

    commit

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from auth.auth_services import create_user_service, login_user_service, logout_user_service
 import inquirer
 from state.auth_state import user_state
+from auth.auth_services import user_credientials_input
 
 state = user_state()
 
 while True:
     user = state.get_user()
 
+
+    
     if user:
         choices = ["logout_user"]
+
     else:
         choices = ["register_user", "login_user"]
 
